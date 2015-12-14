5dca9f0615db0311529383f42d6844a3c9a930ef
bcemmett <bcemmett@users.noreply.github.com>
2015-12-14 20:16:29 +0000

    Find possible row layouts given an input

--- GchqXmasPuzzle/Form1.cs
+++ GchqXmasPuzzle/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -14,12 +15,67 @@ namespace GchqXmasPuzzle
         private void button_Go_Click(object sender, EventArgs e)
         {
             int gridSize = 25;
+
+            List<int[]> arrangements = CalculatePossibleArrangements(new int[] { 7,3,1,1,7 }, gridSize);
+
             CellState[][] grid = new CellState[gridSize][];
             for (int i = 0; i < gridSize; i++)
             {
                 grid[i] = new CellState[gridSize];
             }
             PrintGrid(grid);
+        }
+
+        private List<int[]> CalculatePossibleArrangements(int[] constraint, int gridSize)
+        {
+            List<int[]> acceptableArrangements = new List<int[]>();
+            int[] arrangement = new int[constraint.Length];
+            int[] furthestAccpetablePositions = new int[constraint.Length];
+
+            //Find right-most acceptable positions
+            furthestAccpetablePositions[constraint.Length - 1] = gridSize - constraint[constraint.Length - 1];
+            for (int i = constraint.Length - 2; i >= 0; i--)
+            {
+                furthestAccpetablePositions[i] = furthestAccpetablePositions[i + 1] - constraint[i] - 1;
+            }
+            
+            //Find a first arrangement, as far to the left as possible
+            arrangement[0] = 0;
+            for (int i = 1; i < constraint.Length; i++)
+            {
+                arrangement[i] = arrangement[i - 1] + constraint[i - 1] + 1;
+            }
+            acceptableArrangements.Add(arrangement);
+
+            bool moreArrangements = true;
+
+            //Starting from the right hand side, try to move runs to the right
+            int j = constraint.Length - 1;
+            while (moreArrangements)
+            {
+                arrangement = (int[])arrangement.Clone();
+                if (arrangement[j] < furthestAccpetablePositions[j])
+                {
+                    arrangement[j]++;
+                    //Everything to the right of the moved run should move back to its new left-most position
+                    for (int k = j + 1; k < constraint.Length; k++)
+                    {
+                        arrangement[k] = arrangement[k - 1] + constraint[k - 1] + 1;
+                    }
+                    acceptableArrangements.Add(arrangement);
+                    j = constraint.Length - 1;
+                }
+                else
+                {
+                    j--;
+                }
+                if (j == 0 && arrangement[j] == furthestAccpetablePositions[j])
+                {
+                    moreArrangements = false;
+                }
+            }
+
+            return acceptableArrangements;
         }
 
         private void PrintGrid(CellState[][] grid)
